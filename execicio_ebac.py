090dc3496836f1fa24f17e2adee5e86feedfd734
Jonas Franco <minhacontajonas@gmail.com>
2022-05-02 18:18:10 -0300

    Adiconado codigo webscrapy estraindo os top 100 filmes do site IMDB

--- execicio_ebac.py
+++ execicio_ebac.py
@@ -5,3 +5,19 @@ print('Olá Mundo!!!')
 # salvando todos os top 100 melhores filmes desse site e tudo salvo 
 # em um lista em um arquivo.csv.
 
+import scrapy
+
+
+class ImdbSpider(scrapy.Spider):
+    name = 'imdb'
+
+    start_urls = ['https://www.imdb.com/chart/top/?ref_=nv_nv250']
+
+    def parse(self, response):
+        for filmes  in response.css('.titleColumn'):
+            yield {
+                  'titulo': filmes.css('.titleColumn a::text').get(),
+                  'ano': filmes.css('.secondaryInfo ::text').get()[1:-1],
+                  'nota': response.css('strong ::text').get()
+            }
+        pass
