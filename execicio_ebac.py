f52f7840f09027c0db7c31089c9aa76cd342d10b
Jonas Franco <minhacontajonas@gmail.com>
2022-05-02 17:40:03 -0300

    Adicionado explicação sobre o execicio da ebac e o codigo webcrapy o que vai extrair da web

--- execicio_ebac.py
+++ execicio_ebac.py
@@ -1,2 +1,7 @@
 # Meu primeiro execicio no github
 print('Olá Mundo!!!')
+
+# Nesse execicio da ebac vou subir um codigo de um webscrapy do site IMDB
+# salvando todos os top 100 melhores filmes desse site e tudo salvo 
+# em um lista em um arquivo.csv.
+
